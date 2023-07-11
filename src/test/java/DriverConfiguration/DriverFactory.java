ef1a08580f19059b3a55ae9334e54987ec7fc1cd
LuciferX2002 <md.saifansari@gmail.com>
2023-07-11 23:53:14 +0530

    Updated project to handle Browserstack execution

--- src/test/java/DriverConfiguration/DriverFactory.java
+++ src/test/java/DriverConfiguration/DriverFactory.java
@@ -3,8 +3,10 @@ package DriverConfiguration;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,32 +16,59 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ThreadGuard;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
-	
-	public WebDriver getDriver(DriverConfiguration driverConfig, Map<String, String> config) throws MalformedURLException {
-		
+
+	public WebDriver getDriver(DriverConfiguration driverConfig, Map<String, String> config)
+			throws MalformedURLException {
+
 		WebDriver driver;
-		
-		if(driverConfig.getEnv().equalsIgnoreCase("remote")) {
-			String gridURL = config.get("remoteGridUrl");
-			DesiredCapabilities dc = new DesiredCapabilities();
-			dc.setCapability(CapabilityType.BROWSER_NAME, driverConfig.getBrowser());
-			dc.setCapability(CapabilityType.BROWSER_VERSION, driverConfig.getBrowserVersion());
-			dc.setCapability(CapabilityType.PLATFORM_NAME, driverConfig.getOs());
-			dc.setCapability("network", true);
-			dc.setCapability("video", true);
-			dc.setCapability("console", true);
-			dc.setCapability("visual", true);
+
+		if (driverConfig.getEnv().equalsIgnoreCase("remote-Desktop")) {
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability("browserName", "Chrome");
+			capabilities.setCapability("browserVersion", "102.0");
+			HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+			browserstackOptions.put("platformName", "Windows");
+			browserstackOptions.put("osVersion", "11");
+			capabilities.setCapability("bstack:options", browserstackOptions);
+			capabilities.setCapability("browserstack.ie.enablePopups", "true");
+			capabilities.setCapability("browserstack.user", "");
+			capabilities.setCapability("browserstack.key", "");
+			driver = new RemoteWebDriver(new URL("https://hub.browserstack.com/wd/hub"), capabilities);
+		} else if (driverConfig.getEnv().equalsIgnoreCase("remote-Android")) {
 			
-			driver = new RemoteWebDriver(new URL(gridURL), dc);
+			String username = "";
+			String pwd = "";
+			final String url = "https://"+username+":"+pwd+"@hub.browserstack.com/wd/hub";
+			
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+			browserstackOptions.put("browserName", "iPhone");
+			browserstackOptions.put("device", "iPhone 12");
+			browserstackOptions.put("realMobile", "true");
+			capabilities.setCapability("bstack:options", browserstackOptions);
+			driver = new RemoteWebDriver(new URL(url), capabilities);
+		} else if (driverConfig.getEnv().equalsIgnoreCase("remote-iOS")) {
+			MutableCapabilities capabilities = new XCUITestOptions();
+			HashMap<String, String> bstackOptions = new HashMap<>();
+			// bstackOptions.putIfAbsent("source", "cucumber-java:sample-master:v1.2");
+			capabilities.setCapability(CapabilityType.BROWSER_NAME, driverConfig.getBrowser());
+			capabilities.setCapability(CapabilityType.BROWSER_VERSION, driverConfig.getBrowserVersion());
+			capabilities.setCapability(CapabilityType.PLATFORM_NAME, driverConfig.getOs());
+			capabilities.setCapability("browserstack.user", "saifansari_dilHIM");
+			capabilities.setCapability("browserstack.key", "rpmpuMy1YsxWH4jcuBY4");
+			driver = new IOSDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 		} else {
-			switch(driverConfig.getBrowser().toLowerCase()) {
+			switch (driverConfig.getBrowser().toLowerCase()) {
 			case "chrome":
 				WebDriverManager.chromedriver().setup();
-				if(driverConfig.isHeadless())
-				{
+				if (driverConfig.isHeadless()) {
 					ChromeOptions chromeOptions = new ChromeOptions();
 					chromeOptions.addArguments("window-size=1536x721");
 					chromeOptions.addArguments("--disable-gpu");
@@ -48,13 +77,13 @@ public class DriverFactory {
 					DesiredCapabilities dc = new DesiredCapabilities();
 					dc.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 					chromeOptions.merge(dc);
-					driver = ThreadGuard.protect(new ChromeDriver(chromeOptions));			
+					driver = ThreadGuard.protect(new ChromeDriver(chromeOptions));
 				} else {
-					driver = ThreadGuard.protect(new ChromeDriver());	
+					driver = ThreadGuard.protect(new ChromeDriver());
 				}
 				break;
-				default:
-					throw new WebDriverException("No Browser Specified");
+			default:
+				throw new WebDriverException("No Browser Specified");
 			}
 		}
 		driver.manage().window().maximize();
